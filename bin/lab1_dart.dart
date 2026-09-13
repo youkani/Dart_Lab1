b3466f86afa11c61f26464467fffb346c7270ed7
youkani <baaalena08@yandex.ru>
2026-09-13 16:51:44 +0300

    learn dart cycles

--- bin/lab1_dart.dart
+++ bin/lab1_dart.dart
@@ -92,4 +92,43 @@ void main(){
   print(upper);
   List<String> longNames = names.where ((name) =>   name.length > 4).toList();
   print(longNames);
+
+  //Управляющие конструкции
+  int score2 = 85;
+  String grade;
+  if (score2 >= 90) {
+    grade = 'A';
+  } else if (score2 >= 75) {
+    grade = 'B';
+  } else {
+    grade = 'C';
+  }
+  print(grade);
+  String result = score >= 60 ? 'Сдал' : 'Не сдал';
+  print(result);
+
+for (int i=0; i<5; i++) {
+  print(i);
+}
+List<String> fruits3 = ['яблоко', 'банан', 'груша'];
+for (var fruit in fruits3) {
+  print(fruit);
+}
+int n=0;
+while (n<3) {
+  print(n);
+  n++;
+}
+String day = 'Пн';
+switch (day) {
+  case 'Сб':
+  case 'Вс':
+    print('Выходной');
+    break;
+  case 'Пн':
+    print('Начало недели');
+    break;
+  default:
+    print('Рабочий день');
+}
 }
